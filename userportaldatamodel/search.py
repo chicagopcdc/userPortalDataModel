2e2e22152d41eb05798e6d7ac1d27efbdc2b6dba
grugna <graglia01@gmail.com>
2021-02-02 12:35:25 -0600

    fix

--- userportaldatamodel/search.py
+++ userportaldatamodel/search.py
@@ -24,20 +24,6 @@ import json
 
 
 
-class ProjectSearch(Base):
-    __tablename__ = "project_has_search"
-
-    project_id = Column(Integer, ForeignKey("Project.id"), primary_key=True)
-    project = relationship("Project", backref=backref("project_has_search"))
-
-    search_id = Column(Integer, ForeignKey("Search.id"), primary_key=True)
-    search = relationship("Search", backref=backref("project_has_search"))
-
-    created_at = Column(DateTime(timezone=False), server_default=func.now())
-
-
-
-
 
 class Search(Base):
     
@@ -84,7 +70,16 @@ class Search(Base):
         return self.__str__()
 
 
+class ProjectSearch(Base):
+    __tablename__ = "project_has_search"
 
+    project_id = Column(Integer, ForeignKey("Project.id"), primary_key=True)
+    project = relationship("Project", backref=backref("project_has_search"))
+
+    search_id = Column(Integer, ForeignKey("Search.id"), primary_key=True)
+    search = relationship("Search", backref=backref("project_has_search"))
+
+    created_at = Column(DateTime(timezone=False), server_default=func.now())
 
 
 
