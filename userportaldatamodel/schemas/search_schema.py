ca8670194d9a6dd80f4faf878324b0d6fb1a7b12
Debra Venckus <dvenckus@uchicago.edu>
2021-12-09 10:29:54 -0600

    extend_data_request_model serialize Search class

--- userportaldatamodel/schemas/search_schema.py
+++ userportaldatamodel/schemas/search_schema.py
@@ -1,8 +1,15 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from userportaldatamodel.models import Search
+from marshmallow_sqlalchemy import Nested
+from userportaldatamodel.models import FilterSourceType, Search
+
+
+class FilterSourceType(SQLAlchemyAutoSchema):
+    class Meta:
+        model = FilterSourceType
 
 
 class SearchSchema(SQLAlchemyAutoSchema):
+    filter_source = Nested('FilterSourceType')
+
     class Meta:
         model = Search
-
