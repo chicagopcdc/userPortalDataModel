8ca4f313d2499e9c67a0277eb6bc97076c548110
Debra Venckus <dvenckus@uchicago.edu>
2021-12-14 15:53:15 -0600

    extend_data_request_model expound on search_schema comments

--- userportaldatamodel/schemas/search_schema.py
+++ userportaldatamodel/schemas/search_schema.py
@@ -1,14 +1,16 @@
 from enum import Enum
 
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow_enum import EnumField
-from userportaldatamodel.models import FilterSourceType, Search
+# from marshmallow_enum import EnumField
+# from userportaldatamodel.models import FilterSourceType, Search
+from userportaldatamodel.models import Search
 
-
-# class FilterSourceTypeSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = FilterSourceType
-
+# Began trying to serialize SearchSchema and FilterSourceType (Enum)
+# but ran into difficulties as marshmallow_enum wanted an sqla_session defined
+# and I wasn't certain of the impact that would have with other Gen3 apps using
+# this package.
+# There might be another method for marshmallow enum serializing.
+# Left this here for whomever works on it next
 
 class SearchSchema(SQLAlchemyAutoSchema):
     # filter_source = EnumField(FilterSourceType, by_value=True)
