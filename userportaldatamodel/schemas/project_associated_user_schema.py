116a5983c4af05ead10369917a8d820d4c093819
paulmurdoch19 <paulmurdoch19@gmail.com>
2023-11-03 10:55:17 -0700

    updated schema

--- userportaldatamodel/schemas/project_associated_user_schema.py
+++ userportaldatamodel/schemas/project_associated_user_schema.py
@@ -5,6 +5,7 @@ from marshmallow_sqlalchemy.fields import Nested
 
 class ProjectAssociatedUserSchema(SQLAlchemyAutoSchema):
     associated_user = Nested('AssociatedUserSchema')
+    role = Nested('AssociatedUserRolesSchema')
 
     class Meta:
         model = ProjectAssociatedUser
