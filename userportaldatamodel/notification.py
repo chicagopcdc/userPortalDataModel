790146e49d83c782fd8676515b5f35df04fa05ce
paulmurdoch19 <paulmurdoch19@gmail.com>
2024-10-31 11:43:41 -0700

    update default value type

--- userportaldatamodel/notification.py
+++ userportaldatamodel/notification.py
@@ -14,7 +14,7 @@ class Notification(Base):
 
     notification_id = Column(Integer, ForeignKey("notification_log.id"), primary_key=True, nullable=False) 
     user_id = Column(Integer, primary_key=True, nullable=False)
-    seen = Column(Boolean, nullable=False, server_default='false')
+    seen = Column(Boolean, nullable=False, default=True)
 
     notification_log = relationship("NotificationLog", backref="notification")
 
