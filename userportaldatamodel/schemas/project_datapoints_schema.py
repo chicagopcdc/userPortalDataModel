d12124781d5cfa8615c216b885ad0d9da93c637b
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2025-05-31 11:17:33 -0500

    Update project_datapoints_schema.py
    fixed a bug throwing error

--- userportaldatamodel/schemas/project_datapoints_schema.py
+++ userportaldatamodel/schemas/project_datapoints_schema.py
@@ -1,10 +1,10 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from userportaldatamodel.models import ProjectDataPoints
 from marshmallow_sqlalchemy.fields import Nested
-from amanuensis.schema import ProjectSchema  # Make sure this import is correct
+from amanuensis.schema import ProjectSchema
 
 class ProjectDataPointsSchema(SQLAlchemyAutoSchema):
-    project = Nested(ProjectSchema)  # Use the actual schema class, not a string
+    project = Nested(ProjectSchema)
     
     class Meta:
         model = ProjectDataPoints
