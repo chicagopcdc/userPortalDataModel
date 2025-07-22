121bfb5052b530aa31b7c5d4870a834a1b89be7c
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2025-07-22 10:54:34 -0500

    Update project_datapoints_schema.py

--- userportaldatamodel/schemas/project_datapoints_schema.py
+++ userportaldatamodel/schemas/project_datapoints_schema.py
@@ -1,10 +1,9 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from userportaldatamodel.models import ProjectDataPoints
 from marshmallow_sqlalchemy.fields import Nested
-from amanuensis.schema import ProjectSchema
 
 class ProjectDataPointsSchema(SQLAlchemyAutoSchema):
-    project = Nested(ProjectSchema)
+    project = Nested('ProjectSchema')
     
     class Meta:
         model = ProjectDataPoints
